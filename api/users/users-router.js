6072414671a1425b7c90a47b88f85809c46640a6
sean-mahan <seanmahansean@gmail.com>
2021-09-30 01:04:02 -0500

    gets mostly done

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const User = require("./users-model")
+const Post = require("../posts/posts-model")
 const {
   logger,
   validatePost,
@@ -8,32 +10,45 @@ const {
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  User.get()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(next)
+})
+
+router.get('/:id', validateUserId, (req, res, next) => {
+  if()
+  res.json(req.user)
+})
+
+router.post('/', validateUser, (req, res, next) => {
 
 })
 
-router.get('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
 
 })
 
-router.post('/', validateUser, (req, res) => {
+router.delete('/:id', validateUserId, validateUser, (req, res, next) => {
 
 })
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res, next) => {
 
 })
 
-router.delete('/:id', validateUserId, validateUser, (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res, next) => {
 
 })
 
-router.get('/:id/posts', validateUserId, (req, res) => {
-
-})
-
-router.post('/:id/posts', validateUserId, (req, res) => {
-
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    message: "your code is in pain",
+    err: err.message,
+    stack: err.stack
+  })
 })
 
 module.exports = router
