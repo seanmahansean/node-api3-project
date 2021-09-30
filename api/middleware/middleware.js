2fac8e2a7b00cc2e663ee8ecd235e0ffca1ee33c
sean-mahan <seanmahansean@gmail.com>
2021-09-30 00:29:49 -0500

    validateUserId looking good

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,15 +1,42 @@
+const User = require("../users/users-model")
+
 function logger(req, res, next) {
-
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
-
+async function validateUserId(req, res, next) {
+  try{
+    const user = await User.getById(req.params.id)
+    if(!user){
+      res.status(404).json({
+        message: "User does not exist"
+      })
+    }else{
+      req.user = user
+      next()
+    }
+  }catch(err){
+    res.status(500).json({
+      message: "Error finding user"
+    })
+  }
 }
 
-function validateUser(req, res, next) {
-
+async function validateUser(req, res, next) {
+  next()
 }
 
-function validatePost(req, res, next) {
+async function validatePost(req, res, next) {
+  next()
+}
 
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
 }
--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-
+const {logger} = require("./middleware/middleware")
 const server = express()
+const usersRouter = require("./users/users-router")
 
 server.use(express.json())
+server.use(logger)
+server.use("/api/users", usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
